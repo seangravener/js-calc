e8f6b121e2b060c8a26d3bab5ca30fe90f02eeeb
Seán Gravener <sean@gravener.net>
2020-12-02 14:10:48 -0500

    wip: Modules

--- src/lib/module.js
+++ src/lib/module.js
@@ -1,12 +1,23 @@
 import { events } from "./index.js";
 
-let _module = [];
+let _modules = [];
+// load module into "store" or "state"? no.
+// modules[id]
 
 class Module {
-  constructor(component) {}
+  constructor(module) {
+    // _modules.push(module)
+    this.module = module
+  }
+
+  hi(text, ...args) {
+    console.log(`hi ${text}`, ...args)
+  }
 
   run() {
-    return _module || (_module = new Module(_module));
+    _modules[this.module.id] = this.module
+    // add or link "module"/"component" input/output with a global app state[]. module output is it's state, methods, etc.
+    // return _module || (_module = new Module(_module));
   }
 }
 
