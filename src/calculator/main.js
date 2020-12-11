c87d7ac05a7797a917300b1f940e97807193ebfe
Seán Gravener <sean@gravener.net>
2020-12-10 22:25:52 -0500

    Shuffle 💃

--- src/calculator/main.js
+++ src/calculator/main.js
@@ -3,49 +3,53 @@ import { Calculator } from "./ui/index.js";
 
 const calculator = new Calculator();
 
-calculator.memory.operandB = 2;
-calculator.memory.operator = "+";
+const doCalc = () => {
+  calculator.memory.operandB = 12;
+  calculator.memory.operator = "+";
 
-// on number key:
-let keyType = "number";
-if (keyType === "number") {
+  // on number key:
+  let keyType = "number";
+  if (keyType === "number") {
+    calculator.memory.save();
+  }
+
+  calculator.memory.operandB = 3;
+  calculator.memory.operator = "+";
+  calculator.memory.operator = "-";
+  calculator.memory.operandB = 2;
+  calculator.memory.operator = "/";
+  calculator.memory.operandB = 3;
+  calculator.memory.operator = "*";
+  calculator.memory.operator = "+";
+  calculator.memory.operandB = 2;
+
+  // on number key:
+  if (keyType === "number") {
+    calculator.memory.save();
+  }
+  // set state
+  calculator.memory.operandB = 0;
+  calculator.memory.operator = "-";
+  calculator.memory.save();
+
+  calculator.memory.operandB = 2;
+  calculator.memory.operator = "+";
+  calculator.memory.save();
+
+  calculator.memory.operandB = 2;
+  calculator.memory.operator = "-";
+  calculator.memory.save();
+
+  calculator.memory.operandB = 1;
+  calculator.memory.operator = "*";
+  calculator.memory.save();
+
+  calculator.memory.operandB = 3;
+  calculator.memory.operator = "*";
   calculator.memory.save();
 }
 
-calculator.memory.operandB = 3;
-calculator.memory.operator = "+";
-calculator.memory.operator = "-";
-calculator.memory.operandB = 2;
-calculator.memory.operator = "/";
-calculator.memory.operandB = 3;
-calculator.memory.operator = "*";
-calculator.memory.operator = "+";
-calculator.memory.operandB = 2;
-
-// on number key:
-if (keyType === "number") {
-  calculator.memory.save();
-}
-// set state
-calculator.memory.operandB = 1;
-calculator.memory.operator = "*";
-calculator.memory.save();
-
-calculator.memory.operandB = 2;
-calculator.memory.operator = "+";
-calculator.memory.save();
-
-calculator.memory.operandB = 2;
-calculator.memory.operator = "-";
-calculator.memory.save();
-
-calculator.memory.operandB = 1;
-calculator.memory.operator = "*";
-calculator.memory.save();
-
-calculator.memory.operandB = 3;
-calculator.memory.operator = "*";
-calculator.memory.save();
+doCalc()
 
 console.log(calculator.memory);
 export default calculator;
