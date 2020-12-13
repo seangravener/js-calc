8342f414aea97ae1272afdcc945e291c91e1c741
Sean Gravener <Sean.Gravener@tiaabank.com>
2020-12-12 22:11:19 -0500

    Export singleton

--- src/calculator/index.js
+++ src/calculator/index.js
@@ -1,14 +1,18 @@
-import { CalculatorComponent } from "./ui/calculator.js";
+import { CalculatorComponent } from "./component/calculator.component.js";
 import { Memory } from "./data/memory.js";
 import { EventBus } from "./data/events.js";
 import { Inputs } from "./data/inputs.js";
 
-const _instance = new CalculatorComponent({
+const _state = new CalculatorComponent({
   events: EventBus.load(),
   inputs: Inputs.load(),
   memory: Memory.load(),
   // ui: Ui.load(),
 });
 
+const singleton = () => {
+  return new CalculatorComponent(_state)
+};
+
 export { CalculatorComponent }
-export default _instance
+export default singleton()
