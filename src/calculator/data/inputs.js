4fd88d2d6bf4a7849a5e9880785704c42a9eea11
Sean Gravener <Sean.Gravener@tiaabank.com>
2020-12-15 10:54:49 -0500

    Favor local api

--- src/calculator/data/inputs.js
+++ src/calculator/data/inputs.js
@@ -24,7 +24,7 @@ class Inputs {
   }
 
   append(digit) {
-    memory.operandB = `${memory.operandB}${digit}`;
+    this.set({operandB: `${memory.operandB}${digit}`});
   }
 
   backspace(count = 1) {
