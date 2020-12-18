b892d71b4d5258364830d753cc6e105e94300358
Seán Gravener <sean@gravener.net>
2020-12-18 08:17:17 -0500

    Remove nulls

--- src/calculator/components/display/display.template.js
+++ src/calculator/components/display/display.template.js
@@ -1,20 +1,23 @@
 const historyDisplay = (history) => `
   <div class="calc-operation">
-    ${history}
+    ${history.replace("null", "") || ""}
   </div>`;
 
-const mainDisplay = (operator, output) => `
+const mainDisplay = (operator, operandB) => `
   <div class="calc-typed" id="mainDisplay">
-    <span class="operator">${operator}</span> ${output || "- - -"}
+    <span class="operator">${exists(operator)}</span>
+    ${exists(operandB, "- - -")}
   </div>`;
+
+const exists = (value, placeholder = "") => (value ? `${value}` : placeholder);
 
 const compose = (...parts) =>
   parts.reduce((whole, part) => {
     return `${whole}${part}`;
   }, "");
 
-const templateFn = ({ operator, history, output }) => {
-  return compose(historyDisplay(history), mainDisplay(operator, output));
+const templateFn = ({ operator, history, operandB }) => {
+  return compose(historyDisplay(history), mainDisplay(operator, operandB));
 };
 
 export { templateFn };
