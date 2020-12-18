e7a67ba83c7149d8800b695e27d25a4f3ace30f9
Seán Gravener <sean@gravener.net>
2020-12-18 08:17:43 -0500

    Stub inputs:save event

--- src/calculator/components/display/display.component.js
+++ src/calculator/components/display/display.component.js
@@ -1,8 +1,8 @@
+import inputs from "../../data/inputs.js";
+import events from "../../data/events.js";
 import { Component } from "../base/component.js";
 import { css } from "./display.styles.js";
 import { templateFn } from "./display.template.js";
-import inputs from "../../data/inputs.js";
-import events from "../../data/events.js";
 
 class DisplayComponent extends Component {
   constructor() {
@@ -19,7 +19,7 @@ class DisplayComponent extends Component {
     // watch locals for changes with Proxy?
     this.locals = inputs.get()
 
-    events.listenTo("save", (locals) => {
+    events.listenTo("inputs:save", (locals) => {
       console.log('save!', locals)
       this.locals = locals;
       this.render()
